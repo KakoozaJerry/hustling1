8a57abee4834b1b550c50b46a5b14d25739d4716
KakoozaJerry <abrahamkakooza@gmail.com>
2017-08-09 03:57:12 +0200

    Birthday

---
+++ Final-Project/database_setup.py
@@ -0,0 +1,57 @@
+import os
+import sys
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250))
+    password = Column(String(250))
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+            'email':self.email,
+            'password':self.password,
+        }
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    category = Column(String(250))
+    fee = Column(String(8))
+    date = Column(Date)
+    time = Column(Time)
+    register_id = Column(Integer, ForeignKey('users.id'))
+    users = relationship(Users)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+
+Base.metadata.create_all(engine)
